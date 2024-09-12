2560cf7b96273bf1973dbb117a2c30fc3ed53f1b
abdulrahman-2 <abdoattallah3@gmail.com>
2024-09-12 19:20:47 +0300

    add title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,19 @@ import Cart from "../components/layout/Cart";
 
 const Home = ({ data, setCartItems, handleAddToCart }) => {
   return (
-    <div className="my-14 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {data.map((product) => (
-        <Cart
-          key={product.id}
-          product={product}
-          setCartItems={setCartItems}
-          handleAddToCart={handleAddToCart}
-        />
-      ))}
-    </div>
+    <>
+      <h1 className="text-[35px] font-bold text-center my-10">Products</h1>
+      <div className="my-14 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {data.map((product) => (
+          <Cart
+            key={product.id}
+            product={product}
+            setCartItems={setCartItems}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
